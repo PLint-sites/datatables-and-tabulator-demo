ff058a9d3c508845d734bdf97f108dfad3fd0ded
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2019-09-11 16:18:04 +0200

    Add Font Awesome icons to show sorted column

--- src/components/SortableDatatable.vue
+++ src/components/SortableDatatable.vue
@@ -45,22 +45,23 @@
 
     require('moment')
     require('datatables.net-plugins/sorting/datetime-moment')
+    $.fn.dataTable.moment('DD-MM-YYYY'); 
     
     export default {
         name: 'SortableDatatable',
         mounted() {
-            $.fn.dataTable.moment('DD-MM-YYYY'); 
             $('#sortingtable').DataTable({
                 info: false,
                 lengthChange: false,
                 filter: false,
                 paging: false,
+                order: [[2, 'asc']],
             })
         }
     }
 </script>
 
-<style lang="css">
+<style lang="less">
     .container {
         width: 1000px;
         margin-left: auto;
@@ -69,5 +70,31 @@
 
     table th, table td {
         width: 333px;
+    }
+
+    table th {
+        &.sorting_asc::after {
+            font-family: FontAwesome;
+            display: inline-block;
+            content: '\f078';
+            letter-spacing: 10px;
+            position: relative;
+            top: -2px;
+            margin-left: 5px;
+            color: 888;
+            font-size: 14px;
+        }
+
+        &.sorting_desc::after {
+            font-family: FontAwesome;
+            display: inline-block;
+            content: '\f077';
+            letter-spacing: 10px;
+            position: relative;
+            top: -2px;
+            margin-left: 5px;
+            color: 888;
+            font-size: 14px;
+        }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import './fontawesome/css/font-awesome.min.css';
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
