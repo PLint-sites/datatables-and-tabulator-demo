eb827959beae3541301fba56552ae2815bf2000a
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2019-09-11 17:20:06 +0200

    Remove HelloWorld component and add second date sorting format

--- src/components/SortableDatatable.vue
+++ src/components/SortableDatatable.vue
@@ -3,36 +3,42 @@
         <table id="sortingtable">
             <thead>
                 <tr>
-                <th>Name</th>
-                <th>City</th>
-                <th>Date of Birth</th>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Date of Birth</th>
+                    <th>Last login date</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                <td>Jan</td>
-                <td>Sittard, NL</td>
-                <td>31-08-1980</td>
+                    <td>Jan</td>
+                    <td>Sittard, NL</td>
+                    <td>31-08-1980</td>
+                    <td>Saturday, August 17 2019</td>
                 </tr>
                 <tr>
-                <td>Peer</td>
-                <td>Berlin, DE</td>
-                <td>05-09-1980</td>
+                    <td>Peer</td>
+                    <td>Berlin, DE</td>
+                    <td>05-09-1980</td>
+                    <td>Tuesday, September 10 2019</td>
                 </tr>
                 <tr>
-                <td>Paddy</td>
-                <td>Liege, BE</td>
-                <td>25-09-1980</td>
+                    <td>Paddy</td>
+                    <td>Liege, BE</td>
+                    <td>25-09-1980</td>
+                    <td>Wednesday, August 21 2019</td>
                 </tr>
                 <tr>
-                <td>Lim</td>
-                <td>Tokyo, JP</td>
-                <td>03-08-1980</td>
+                    <td>Lim</td>
+                    <td>Tokyo, JP</td>
+                    <td>03-08-1980</td>
+                    <td>Friday, September 6 2019</td>
                 </tr>
                 <tr>
-                <td>Yong</td>
-                <td>Shanghai, CH</td>
-                <td>01-09-1980</td>
+                    <td>Yong</td>
+                    <td>Shanghai, CH</td>
+                    <td>01-09-1980</td>
+                    <td>Monday, September 9 2019</td>
                 </tr>
             </tbody>
         </table>
@@ -40,12 +46,17 @@
 </template>
 
 <script>
+    // Import jQuery into the $ variable, datatables.net needs it this way
     import $ from 'jquery'
-    import dt from 'datatables.net'
+    import 'datatables.net'
 
+    // Import moment and datatables.net plugin for sorting using moment
     import 'moment'
     import 'datatables.net-plugins/sorting/datetime-moment'
+
+    // Define the format of the date column(s)
     $.fn.dataTable.moment('DD-MM-YYYY'); 
+    $.fn.dataTable.moment('dddd, MMMM D YYYY'); 
     
     export default {
         name: 'SortableDatatable',
